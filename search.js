db84ec8fbf609529c680d7877f984fedad9d4bb1
mq20050522 <58794091+mq20050522@users.noreply.github.com>
2022-04-08 09:41:18 -0400

    Update search.js

--- search.js
+++ search.js
@@ -1,3 +1,9 @@
-function search(string){
-    return(string)
+/*
+["(name), (photo), (class), (notes)","(name), (photo), (class), (notes)","(name), (photo), (class), (notes)"]
+
+*/
+listOStudents = ["Ming, ming.jpg, cs, ...", "Josh, josh.png, math, ^_-"]
+function search(keyword, listOStudents){
+    var returnValue = []
+    return(returnValue)
 }
