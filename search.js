236434efd88870ba94ca9c34b2a8de3dcac50439
mq20050522 <58794091+mq20050522@users.noreply.github.com>
2022-04-08 10:09:45 -0400

    completed search function

--- search.js
+++ search.js
@@ -1,9 +1,19 @@
 /*
+old format: 
 ["(name), (photo), (class), (notes)","(name), (photo), (class), (notes)","(name), (photo), (class), (notes)"]
-
+new format: 
+[["name", "photo", "class", "notes"], ["name", "photo", "class", "notes"], ["name", "photo", "class", "notes"]]
 */
-listOStudents = ["Ming, ming.jpg, cs, ...", "Josh, josh.png, math, ^_-"]
-function search(keyword, listOStudents){
+listOStudents = [["Ming", "ming.jpg", "cs", "..."], ["Josh", "josh.png", "math", "^_-"]]
+function search(keywords, listOStudents){
     var returnValue = []
+    for(var i=0; i<listOStudents.length; i++){
+        // console.log(listOStudents[i][0].toLowerCase())
+        if(listOStudents[i][0].toLowerCase().includes(keywords)){
+            returnValue.push(listOStudents[i])
+        }
+    }
     return(returnValue)
 }
+console.log(search("min", listOStudents))
+console.log(search("osh", listOStudents))
