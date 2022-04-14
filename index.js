2258b9a205f2f20b95f618e049182fffbbc450ee
mq20050522 <58794091+mq20050522@users.noreply.github.com>
2022-04-14 09:45:26 -0400

    kill me

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const hostname = '127.0.0.1';
 const port = 3000;
 const server = require('http').createServer(app);
 
+const users = require('./data').userDB
+
 app.use(express.static(__dirname + "/public"));
 app.set('views', __dirname + "/public");
 // app.set('view engine', 'mustache');
