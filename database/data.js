5573bbf3a189957a8cadb358d80b60f665c94d02
mq20050522 <58794091+mq20050522@users.noreply.github.com>
2022-04-14 09:37:00 -0400

    啊啊啊啊啊啊！

--- database/data.js
+++ database/data.js
@@ -1,7 +1,8 @@
 const userDB = [];
 
 function addExistingUsers(){
-    
+    var fs = require('fs');
+    var data = JSON.parse(fs.readFileSync('existingData.json', 'utf-8'));
 }
 
 module.exports = { userDB };
