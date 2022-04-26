508bc13df002cad19e8fb789d15097bb4d3e0f22
mq20050522 <58794091+mq20050522@users.noreply.github.com>
2022-04-26 11:45:32 -0400

    filterd a bunch of babies

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -57,14 +57,42 @@ def getAllContents():
     data['pfp'] = getProfile()
     return data
 
+def getAllFilteredContents():
+    console.log('Generating Json File...')
+    data = {}
+    data['names'] = []
+    data['grades'] = []
+    data['pfp'] = []
+    names = getNames()
+    grades = getGrade()
+    pfp = getProfile()
+    # console.debug(grades)
+    filterList = []
+    for i in range(len(grades)):
+        if grades[i]=='Grade 9' or grades[i]=='Grade 10' or grades[i]=='Grade 11' or grades[i]=='Grade 12': pass
+        else: filterList.append(i)
+    # console.debug(filterList)
+    # console.debug(grades)
+    for i in filterList: 
+        names[i] = 'deleted'
+        grades[i] = 'deleted'
+        pfp[i] = 'deleted'
+    # console.debug(grades)
+    for i in range(len(grades)):
+        if names[i] != 'deleted': data['names'].append(names[i])
+        if grades[i] != 'deleted': data['grades'].append(grades[i])
+        if pfp[i] != 'deleted': data['pfp'].append(pfp[i])
+    # console.debug(data['grades'])
+    return data
+
 def exportJSON():
-    contents = getAllContents()
+    contents = getAllFilteredContents()
     console.log("Writing to file...")
     with open("data.json",'w+') as f:
         json.dump(contents, f)
 
 def exportCSV():
-    contents = getAllContents()
+    contents = getAllFilteredContents()
     console.log("Writing to file...")
     df = pd.DataFrame(contents)
     # with open("data.csv", 'w+') as f:
@@ -72,5 +100,5 @@ def exportCSV():
     df.to_csv('data.csv')
 
 if __name__ == '__main__':
-    # exportJSON()
+    exportJSON()
     exportCSV()
