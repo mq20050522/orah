72e8617c7f8ed4db53a84ff84bf198ed4736963a
mq20050522 <58794091+mq20050522@users.noreply.github.com>
2022-04-26 10:53:28 -0400

    deleted bunch o' stuff

--- database/data.js
+++
@@ -1,8 +0,0 @@
-const userDB = [];
-
-function addExistingUsers(){
-    var fs = require('fs');
-    var data = JSON.parse(fs.readFileSync('existingData.json', 'utf-8'));
-}
-
-module.exports = { userDB };
